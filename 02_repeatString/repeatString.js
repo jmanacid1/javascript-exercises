067ea4e0b0c57338e86cd41e3158c7b5bb1967b9
Jackson <jacksonwlipfert@gmail.com>
2022-06-09 11:42:18 -0400

    first and second tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let returnString = '';
+    for (let i = 0; i < num; i++){
+        returnString.concat(string);
+    }
+    console.log(returnString)
+    return returnString
 };
 
 // Do not edit below this line
