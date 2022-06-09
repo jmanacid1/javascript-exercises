5a7f682e446f6884750cdb3f731b7bd6f84e1237
Jackson <jacksonwlipfert@gmail.com>
2022-06-09 12:21:14 -0400

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string1) {
+    let splitString = [];
+    for (let i = 0; i < string1.length; i++){
+        splitString.push(string1.charAt(i))
+    }
+    let reversedString = splitString.reverse();
+    let joinedString = reversedString.join('');
+    return joinedString;
 };
 
 // Do not edit below this line
