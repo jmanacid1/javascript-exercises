ef8f47ed83a9cf85d4cd6cb5f3c1975a447f96b6
Jackson <jacksonwlipfert@gmail.com>
2022-06-10 14:35:08 -0400

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if(typeof(temp) !== "number"){
+    return 'ERROR'
+  }
+  let cTemp = (temp-32) * (5/9);
+  let roundedCTemp = +cTemp.toFixed(1)
+  return roundedCTemp
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  if(typeof(temp) !== "number"){
+    return 'ERROR'
+  }
+  let fTemp = (temp * (9/5)) + 32;
+  let roundedFTemp = +fTemp.toFixed(1)
+  return roundedFTemp
 
 };
 
